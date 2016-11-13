1ac78734cf2efe08b0a2bfb528b03c87cf362e94
deangelo200 <deangeloalmeus@Deangelos-Macintosh.local>
2016-11-12 23:45:09 -0500

    ffff woee

--- js/animal.js
+++ js/animal.js
@@ -177,17 +177,11 @@ $(".swiper-slide").addClass(function(index){
 
 slidePicture("0","css/animals-img/catfish.jpg");
 slidePicture("1","css/animals-img/catfish.jpg");
-slidePicture("4","../img/animals-img/lobster.jpg");
+slidePicture("2","css/animals-img/catfish.jpg");
 slidePicture("5","img/animals-img/turtle.jpg");
     
 })
 
-$(document).on("pageinit",function(){
-  slidePicture("2","css/animals-img/catfish.jpg");
-    slidePicture("3","../img/animals-img/catfish.jpg")
-    
-    
-});
 
 
 
@@ -244,3 +238,5 @@ $(".picture-background").css({
 });
 }
 */
+
+slidePicture("5","img/animals-img/turtle.jpg");
